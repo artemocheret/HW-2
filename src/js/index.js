db0d6d3bf8b474795279ae173bd1cbb725404255
Artem <artemoceret24@gmail.com>
2025-12-26 16:12:42 +0200

    виконав дз

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+// 1 завдання
+const celsius = 10
+const fahrenheit = (celsius * 9/5) + 32
+console.log(fahrenheit)
+// 2 завдання
+const daysInMonth = 31
+const hoursInMonth = daysInMonth * 24
+const minutesInMonth = hoursInMonth * 60
+console.log(hoursInMonth)
+console.log(minutesInMonth)
+// 3 завдання
+let helth = 100
+let energy = 200
+console.log(helth - 50)
+console.log(energy - 20)
+// 4 завдання
+const totalPrice = 120
+const discountPrice = totalPrice / 100 * 10
+const discountedPrice = totalPrice - discountPrice
+console.log(discountedPrice)
+// 5 завдання
+const floatNumder = 12.78
+const roundedDown = Math.floor(floatNumder)
+console.log(roundedDown)
+// 6 завдання
+const floatString = "45.67"
+const parsedFloat = parseFloat(floatString)
+console.log(parsedFloat)
+// 7 завдання
+const intString = "123"
+const parsedInt = parseInt(intString) 
+console.log(parsedInt)
+// 8 завдання
+const number = 10
+const sqrtNumber = Math.sqrt(number)
+console.log(sqrtNumber)
+// 9 завдання
+const integer = 42
+const stringNumber = "256"
+const convertedInt = parseInt(stringNumber)
+console.log(convertedInt)
+const convertedString = integer.toString() 
+console.log(convertedString)
